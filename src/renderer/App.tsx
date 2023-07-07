5cf30f90ae07a4015f54d29f07af92313012832e
Alexander V <alexandervolkers@gmail.com>
2023-07-07 20:51:21 +0200

    initial commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,16 @@
+// import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return <div>HELLO</div>;
+
+  /* return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  ); */
+}
+
+export default App;
